72f91ce3f4bab5c25c6f0e75dc006f8c9e1e9e7f
rafaelst2000 <rafaelst2000@gmail.com>
2021-05-04 19:58:55 -0300

    styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import styled from "styled-components";
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Title>Hello world</Title>
     </div>
   );
 }
