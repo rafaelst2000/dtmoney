f15e105210bb26ac1911cf7aca86b5c2a0dbd8eb
Rafael Souza Trevisan <rafael.trevisan@goclin.com>
2021-06-19 11:15:38 -0300

    passing props to styled components

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -51,32 +51,35 @@ export const TransactionTypeContainer = styled.div `
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
+`
+interface RadioBoxProps {
+  isActive: boolean
+}
 
-  button{
-    height: 4rem;
-    border: 1px solid #d7c7d7;
-    border-radius: 0.25rem;
-    background: transparent;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+export const RadioBox = styled.button <RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7c7d7;
+  border-radius: 0.25rem;
+  background: ${(props) => props.isActive ? '#eee' : 'transparent'};
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    transition: border-color 0.2;
+  transition: border-color 0.2;
 
-    :hover{
-      border-color: ${darken(0.1, '#d7c7d7')}
-    }
+  :hover{
+    border-color: ${darken(0.1, '#d7c7d7')}
+  }
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  img {
+    width: 20px;
+    height: 20px;
+  }
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `
